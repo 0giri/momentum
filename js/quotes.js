09194141844c5677b47fc15e7eaf2ff9e8938901
0giri <higiri@naver.com>
2021-09-03 00:31:03 +0900

    directory update, add quote,img

---
+++ js/quotes.js
@@ -0,0 +1,57 @@
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const quotes = [
+  {
+    quote:
+      "교묘하다는 것은 서툰 것만 못하다. 약삭빠른 것보다는 오히려 우직한 것이 더 났다.",
+    author: "회남자",
+  },
+  {
+    quote: "그릇이 차면 넘치고, 사람이 자만하면 이지러진다.",
+    author: "명심보감",
+  },
+  {
+    quote: "모욕은 잊어버리고, 친절은 결코 잊지 말아라.",
+    author: "공자",
+  },
+  {
+    quote: "스스로 돌아봐서 잘못이 없다면 천만인이 가로막아도 나는 가리라.",
+    author: "맹자",
+  },
+  {
+    quote: "아는 자는 말하지 않고, 지껄이는 자는 알지 못한다.",
+    author: "노자",
+  },
+  {
+    quote: "우리가 넘어지는 것은 다리의 잘못이 아니다.",
+    author: "중국 격언",
+  },
+  {
+    quote:
+      "아는 것은 어려운 것이 아니다. 알고 있는 것을 실행하는 것이 어려운 것이다.",
+    author: "서경",
+  },
+  {
+    quote: "욕심이 많은 자는 양심이 얕은 자이다.",
+    author: "장자",
+  },
+  {
+    quote: "의심스러운 사람은 쓰지 말고, 사람을 썼거든 의심하지 말라.",
+    author: "명심보감",
+  },
+  {
+    quote:
+      "물이 너무 깨끗하면 고기가 살지 않고, 사람이 너무 살피게 되면 인재가 모여들지 않는다.",
+    author: "고시",
+  },
+  {
+    quote: "무슨 일이든지 한 가지 일에 능통하라.",
+    author: "경행록",
+  },
+];
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todayQuote.quote;
+author.innerText = todayQuote.author;
